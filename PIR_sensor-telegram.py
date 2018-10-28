88f508abc93ffde4e62451d342eab76abe6ad96e
Leonardo Waite <38167838+lwaite@users.noreply.github.com>
2018-10-27 22:23:38 -0300

    Add files via upload

---
+++ PIR_sensor-telegram.py
@@ -0,0 +1,29 @@
+import serial
+import telegram
+import time
+
+myID = '533544103'
+bot = telegram.Bot(token=Token)
+ser = serial.Serial('/dev/ttyACM0',9600)
+
+def sendMessage():
+    try:
+        bot.sendMessage(myID, 'Presença detectada')
+        time.sleep(3)
+    except:
+        pass
+
+def waitPresence():
+    while True:
+        read = ser.readline()
+        message = read.decode("utf-8")
+        print(message)
+        if message != '' :
+            sendMessage()
+        time.sleep(0.5)    
+
+def main():
+    waitPresence()
+
+if __name__ == '__main__':
+    main()
